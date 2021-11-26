6bbe76a8292f9d0ae544f90422e2ff294dbdd13f
Ole <36864120+olejorga@users.noreply.github.com>
2021-11-26 17:15:57 +0100

    [FEAT] Preliminary cpp file

---
+++ otto_throttle.cpp
@@ -0,0 +1,21 @@
+// TODO: MANY INCLUDE THINGS!
+
+
+void adjust_thrust() {
+    return 0;
+}
+
+
+void increase_thrust() {
+    return 0;
+}
+
+
+void decrease_thrust() {
+    return 0;
+}
+
+
+int main() {
+    return 0;
+}
