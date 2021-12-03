deba0f612d1f71a60858c8bd4dfe1d93599d4f1d
olejorga <olejorga@icloud.com>
2021-12-03 21:21:20 +0100

    [FEAT] Added files from native!

---
+++ README.md
@@ -0,0 +1,27 @@
+# Otto Throttle
+A basic auto-throttle for X-Plane 11, written with love in C++ <3
+
+Tired of manually adjusting your throttle in cruise, when all you want to do is make a cup of coffee? 
+Afraid something might go wrong while you´re gone? 
+Don´t worry! Otto is here to take care of you.
+
+Otto-throttle is a basic auto-throttle script that adjusts your thrust based on your selected speed.
+
+Intended to be used in cruise with the FlyJSim 732 & 722.
+
+[Download from GitHub](https://github.com/olejorga/otto-throttle/releases)
+
+## Requirements
+- X-Plane 11.xx > 11.50
+
+## Installation
+Place the extracted "otto_throttle" folder inside your plugins folder
+```bash
+C:\<PATH TO XP>\X-Plane 11\Resources\plugins\otto_throttle
+```
+
+## Usage
+Can be toggled on or off via the plugins menu:
+```
+Plugins > Otto Throttle > Engage
+```
---
+++ otto_throttle.cpp
@@ -0,0 +1,165 @@
+#include <string>
+#include <cstdlib>
+#include <cstring>
+
+#include "XPLMMenus.h"
+#include "XPLMDataAccess.h"
+#include "XPLMProcessing.h"
+#include "XPLMUtilities.h"
+
+
+#if IBM
+	#include <windows.h>
+#endif
+
+#ifndef XPLM300
+	#error This is made to be compiled against the XPLM300 SDK
+#endif
+
+
+int g_is_engaged = 0;
+int g_menu_container_idx;
+
+XPLMMenuID g_menu_id;
+
+XPLMDataRef g_current_speed = XPLMFindDataRef("sim/flightmodel/position/indicated_airspeed"); // FLOAT
+XPLMDataRef g_target_speed = XPLMFindDataRef("sim/cockpit2/autopilot/airspeed_dial_kts_mach"); // FLOAT
+XPLMDataRef g_throttle_setting = XPLMFindDataRef("sim/cockpit2/engine/actuators/throttle_ratio_all"); // FLOAT
+XPLMDataRef g_sim_rate = XPLMFindDataRef("sim/time/sim_speed"); // INT
+
+float g_last_speed = XPLMGetDataf(g_current_speed);
+
+
+void menu_handler(void*, void*);
+void inflate_otto(void);
+float adjust_thrust(float elapsed1, float elapsed2, int ctr, void* refcon);
+void increase_thrust(float factor);
+void decrease_thrust(float factor);
+
+
+PLUGIN_API int XPluginStart(
+	char* outName,
+	char* outSig,
+	char* outDesc)
+{
+	strcpy(outName, "Otto Throttle");
+	strcpy(outSig, "olejorga");
+	strcpy(outDesc, "A sample plug-in that demonstrates and exercises the X-Plane menu API.");
+
+	inflate_otto();
+
+	return 1;
+}
+
+
+PLUGIN_API void	XPluginStop(void) { }
+
+
+PLUGIN_API void XPluginDisable(void) { }
+
+
+PLUGIN_API int XPluginEnable(void) { return 1; }
+
+
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void* inParam) { }
+
+
+void menu_handler(void* in_menu_ref, void* in_item_ref)
+{
+	if (!strcmp((const char*)in_item_ref, "engage"))
+	{
+		if (g_is_engaged == 0) 
+		{
+			g_is_engaged = 1;
+			XPLMSetMenuItemName(g_menu_id, 0, "Disengage O/T", 0);
+		} 
+		else if (g_is_engaged == 1)
+		{
+			g_is_engaged = 0;
+			XPLMSetMenuItemName(g_menu_id, 0, "Engage O/T", 0);
+		}
+	}
+}
+
+
+void inflate_otto(void)
+{
+	g_menu_container_idx = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "Otto Throttle", 0, 0);
+	g_menu_id = XPLMCreateMenu("Sample Menu", XPLMFindPluginsMenu(), g_menu_container_idx, menu_handler, NULL);
+
+	XPLMAppendMenuItem(g_menu_id, "Engage O/T", (void*)"engage", 1);
+
+	XPLMRegisterFlightLoopCallback(adjust_thrust, 1, NULL);
+}
+
+
+void deflate_otto(void)
+{
+	XPLMDestroyMenu(g_menu_id);
+	XPLMRemoveMenuItem(XPLMFindPluginsMenu(), g_menu_container_idx);
+}
+
+
+float adjust_thrust(float elapsed1, float elapsed2, int ctr, void* refcon)
+{
+	if (g_is_engaged == 1 && XPLMGetDatai(g_sim_rate) != 0)
+	{
+		float speed_diff = abs(XPLMGetDataf(g_current_speed) - XPLMGetDataf(g_target_speed));
+		float current_speed = XPLMGetDataf(g_current_speed);
+		float target_speed = XPLMGetDataf(g_target_speed);
+
+		if ((current_speed < target_speed) && (current_speed < g_last_speed))
+		{
+			if (speed_diff < 5)
+			{
+				increase_thrust(0.0001);
+			}
+			else {
+				increase_thrust(0.001);
+			}
+		}
+
+		if ((current_speed > target_speed) && (current_speed > g_last_speed))
+		{
+			if (speed_diff < 5)
+			{
+				decrease_thrust(0.0001);
+			}
+			else {
+				decrease_thrust(0.001);
+			}
+		}
+
+		g_last_speed = current_speed;
+	}
+
+	return -1;
+}
+
+
+void increase_thrust(float factor)
+{
+	float throttle_setting = XPLMGetDataf(g_throttle_setting);
+
+	if ((throttle_setting + factor) == 1.0)
+	{
+		XPLMSetDataf(g_throttle_setting, 1.0);
+	}
+	else if (throttle_setting < 1) {
+		XPLMSetDataf(g_throttle_setting, (throttle_setting + factor));
+	}
+}
+
+
+void decrease_thrust(float factor)
+{
+	float throttle_setting = XPLMGetDataf(g_throttle_setting);
+
+	if ((throttle_setting - factor) == 1.0)
+	{
+		XPLMSetDataf(g_throttle_setting, 0.0);
+	}
+	else if (throttle_setting > 0) {
+		XPLMSetDataf(g_throttle_setting, (throttle_setting - factor));
+	}
+}
