ae0aa58c31fdad4af8a53767570f2c7f2263d5f4
olejorga <olejorga@icloud.com>
2021-12-03 19:57:58 +0100

    [FIX] Fixed menu

--- otto_throttle.cpp
+++ otto_throttle.cpp
@@ -48,7 +48,8 @@ PLUGIN_API int XPluginStart(
 	g_menu_container_idx = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "Otto Throttle", 0, 0);
 	g_menu_id = XPLMCreateMenu("Sample Menu", XPLMFindPluginsMenu(), g_menu_container_idx, menu_handler, NULL);
 
-	XPLMAppendMenuItem(g_menu_id, "Engage O/T", (void*)"switch", 1);
+	XPLMAppendMenuItem(g_menu_id, "Engage O/T", (void*)"engage", 1);
+	XPLMCheckMenuItemState(g_menu_id, 0, (XPLMMenuCheck*)1);
 
 	XPLMRegisterFlightLoopCallback(adjust_thrust, 1, NULL);
 
@@ -81,16 +82,16 @@ PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void* inPa
 
 void menu_handler(void* in_menu_ref, void* in_item_ref)
 {
-	if (!strcmp((const char*)in_item_ref, "switch") && g_is_engaged == 0)
+	if (!strcmp((const char*)in_item_ref, "engage") && g_is_engaged == 0)
 	{
 		g_is_engaged = 1;
-		XPLMSetMenuItemName(g_menu_id, 0, "Disengage O/T", 0);
+		XPLMCheckMenuItemState(g_menu_id, 0, (XPLMMenuCheck*)2);
 	}
 	
-	if (!strcmp((const char*)in_item_ref, "switch") && g_is_engaged == 1)
+	if (!strcmp((const char*)in_item_ref, "engage") && g_is_engaged == 1)
 	{
 		g_is_engaged = 0;
-		XPLMSetMenuItemName(g_menu_id, 0, "Engage O/T", 0);
+		XPLMCheckMenuItemState(g_menu_id, 0, (XPLMMenuCheck*)1);
 	}
 }
 
