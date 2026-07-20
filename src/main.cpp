ca5970cc8d70f93014d836c17c63250211cf16b9
Ole-Jørgen Andersen <olejorga@icloud.com>
2026-07-20 21:15:08 +0200

    Just some cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,14 @@
 #include <XPLMPlugin.h>
 #include <cstring>
 
-extern "C" {
-
-PLUGIN_API int XPluginStart(char* outName,
-                            char* outSig,
-                            char* outDesc)
-{
+PLUGIN_API int XPluginStart(char* outName, char* outSig, char* outDesc) {
     std::strcpy(outName, "Otto Throttle");
-    std::strcpy(outSig, "com.example.otto-throttle");
-    std::strcpy(outDesc, "This is version 3.0");
+    std::strcpy(outSig, "com.github.otto-throttle");
+    std::strcpy(outDesc, "A PID based auto-throttle for X-Plane 12");
     return 1;
 }
 
 PLUGIN_API void XPluginStop() {}
-
 PLUGIN_API int XPluginEnable() { return 1; }
-
 PLUGIN_API void XPluginDisable() {}
-
-PLUGIN_API void XPluginReceiveMessage(XPLMPluginID,
-                                      int,
-                                      void*)
-{
-}
-
-}
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID, int, void*) {}
